9ba0e1912223e8fe48ef39d08310f37d978276ba
Robert Maxwell <robertjamesmaxwell@gmail.com>
2020-07-22 23:09:26 -0500

    flesh out avl

--- bholt-cs-101/data-structures/avl-tree.js
+++ bholt-cs-101/data-structures/avl-tree.js
@@ -54,14 +54,76 @@ class Node {
   balance() {
     const rightHeight = this.right ? this.right.height : 0;
     const leftHeight = this.left ? this.left.height : 0;
+
+    // checks if tree is out of balance on left
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
+
+      // checks unbalance is skewed and if we need the additional secondary rotation
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR();
+      }
+
+      // performs rotation to restore balance
+      this.rotateLL();
+
+      // checks if tree is out of balance on right
+    } else if (rightHeight > leftHeight + 1) {
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
+      const rightRightHeight = this.right.right ? this.right.right.height : 0;
+
+      // checks unbalance is skewed and if we need the additional secondary rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      // performs rotation to restore balance
+      this.rotateRR();
+    }
+  }
+
+  rotateRR() {
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
+    this.left.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  rotateLL() {
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+    this.right.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  updateInNewLocation() {
+    if (!this.right && !this.left) {
+      this.height = 1;
+    } else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1;
+    } else if (!this.left || this.right.height > this.left.height) {
+      this.height = this.right.height + 1;
+    }
   }
 }
 
 // console.log(new Node());
 const tree = new AVLTree();
-tree.add(4);
-tree.add(3);
-tree.add(2);
-tree.add(5);
-tree.add(6);
+[...Array(100).keys()].forEach((key) => tree.add(key));
 console.log(tree.getTree());
